356aa8e00f2ac0b8c078de4dd0bb35e642256222
Tim Clarke <tim.clarke@minerva.info>
2020-02-09 21:26:45 +0000

    working initial test

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ __license__ = "Private"
 __version__ = "0.0.1"
 
 """
-integrumfeed.py
+test.py
 
   created by:   Tim Clarke
   date:         8jan2020
   change log:
-  purpose:      test pyqt
+  purpose:      testing of pyqt
 
                 pseudo-code:
                   
@@ -24,128 +24,70 @@ integrumfeed.py
 """
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
-
-#Custom designed pyqtwidgets
-#Daryl W. Bennett --kd8bny@gmail.com
-#Purpose is to have a custom UI
-
-#R1
-
-from PyQt5.QtGui import QWidget
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt5.QtGui import QPainter, QBrush, QColor
+from PyQt5.QtCore import Qt, QPoint 
+# from PyQt5.QtGui import QIcon
+# from PyQt5.QtCore import pyqtSlot
 
 
-class speedowidget(QtGui.QWidget):
+class MainWindow(QWidget):
+   def __init__(self):
+      super().__init__()
+      self.initWindow()
 
-    speedChange = QtCore.pyqtSignal(int)
+   def initWindow(self):
+      self.setGeometry(100,100,400,280)
+      self.setWindowTitle("PyQt5 testbed")
+      self.show()
 
-    def __init__(self, parent=None):
-        super(speedowidget, self).__init__(parent)
+   def paintEvent(self, event):
+      qp = QPainter()
+      qp.begin(self)
+      app.setStyle('Fusion')
+      widget = QWidget(self)
+      
+      button1 = QPushButton(widget)
+      button1.setText("Button1")
+      button1.move(64,32)
+      button1.clicked.connect(button1_clicked)
 
-        self.speedChange.connect(self.update)
-        self.speedChange.connect(self.updateValue)
+      button2 = QPushButton(widget)
+      button2.setText("Button2")
+      button2.move(64,64)
+      button2.clicked.connect(button2_clicked)
 
-        self.setWindowTitle(QtCore.QObject.tr(self, "kd8bny Speedometer"))
-        self.resize(200, 200)
+      label = QLabel(widget)
+      font = QtGui.QFont()
+      font.setBold(True)
+      label.setFont(font)
+      label.setText('example')
 
-        self.rect = QtCore.QRect(-50,-50,100,100)
-        self.startAngle = 0 * 16
-        self.spanAngle = 180 * 16     
+      qp = QPainter(widget)
+      qp.begin(self)
+      colour = QColor(0, 0, 0)
+      colour.setNamedColor('#d4d4d4')
+      qp.setPen(colour)
+      qp.setBrush(QColor(200, 0, 0))
+      qp.drawRect(250, 100, 50, 50)
+      qp.end()
 
-        self.needle = QtGui.QPolygon([
-            QtCore.QPoint(1, 0),
-            QtCore.QPoint(0, 1),
-            QtCore.QPoint(-1, 0),
-            QtCore.QPoint(0, -50)
-            ])
+      widget.show()
 
-        self.backColor = QtGui.QColor('white')
-        self.needleColor = QtGui.QColor('orange')
-        self.tickColor = QtGui.QColor('red')
-
-        self.speed = 0    
-
-    def paintEvent(self, event):
-        side = min(self.width(), self.height()) 
-        qtpaint = QtGui.QPainter()
-
-        qtpaint.begin(self)
-
-        qtpaint.setRenderHint(QtGui.QPainter.Antialiasing)
-        qtpaint.translate(self.width()/2, self.height()/2)
-        qtpaint.scale(side / 120.0, side / 120.0)
-
-        #Background
-        qtpaint.setPen(QtGui.QColor('black'))
-        qtpaint.setBrush(QtGui.QBrush(self.backColor))
-
-        qtpaint.drawChord(self.rect, self.startAngle, self.spanAngle)
-        qtpaint.save()
-
-        #Needle
-        qtpaint.setPen(QtCore.Qt.NoPen)
-        qtpaint.setBrush(QtGui.QBrush(self.needleColor))
-        qtpaint.rotate(-90 + (self.speed/10) * 15)
-        
-        qtpaint.drawConvexPolygon(self.needle)
-        qtpaint.restore()
-
-        #Tick marks
-        qtpaint.setPen(self.tickColor)
-
-        qtpaint.rotate(-15.0)
-        for i in range(0, 11):
-            qtpaint.drawLine(50, 0, 47, 0)
-            qtpaint.rotate(-15.0)
-
-        qtpaint.end()
-
-        return
-
-    @QtCore.pyqtSlot(int)
-    def updateValue(self, speed):
-        if speed >= 0 and speed <= 120:
-            self.speed = speed
-            self.update()
-
-        return
-
-if __name__ == "__main__":
-    import sys
-
-    app = QtGui.QApplication(sys.argv)
-    widget = speedowidget()
-    widget.show()
-    widget.speedChange.emit(60)
-    sys.exit(app.exec_())
-
-def window():
-   app = QApplication(sys.argv)
-   widget = QWidget()
-   
-   button1 = QPushButton(widget)
-   button1.setText("Button1")
-   button1.move(64,32)
-   button1.clicked.connect(button1_clicked)
-
-   button2 = QPushButton(widget)
-   button2.setText("Button2")
-   button2.move(64,64)
-   button2.clicked.connect(button2_clicked)
-
-   widget.setGeometry(50,50,320,200)
-   widget.setWindowTitle("PyQt5 Button Click Example")
-   widget.show()
-   sys.exit(app.exec_())
 
 
 def button1_clicked():
    print("Button 1 clicked")
 
+
+
 def button2_clicked():
-   print("Button 2 clicked")   
-   
+   print("Button 2 clicked")
+
+
+
 if __name__ == '__main__':
-   window()
+   app = QApplication(sys.argv)
+   ex = MainWindow()
+   sys.exit(app.exec_())
