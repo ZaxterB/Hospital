a8a1e0642ea35bbc3004215eecb2d6041a94c11d
Tim Clarke <tim.clarke@minerva.info>
2020-02-16 13:54:54 +0000

    first commit after re-organising file tree

--- src/main.py
+++ src/main.py
@@ -1,9 +1,51 @@
-from PyQt5.QtWidgets import *
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""MOD005622 TRI2 B01CAM"""
+__author__ = "Tim Clarke/Zach Beed"
+__copyright__ = "Copyright 2020, Tim Clarke/Zach Beed"
+__license__ = "Private"
+__version__ = "0.0.2"
 
-if __name__ == "__main__":
-    app = QApplication([])
-    label = QLabel('Hello World!')
-    label.show()
-    label2 = QLabel('I am a stegosaurus!')
-    label2.show()
-    app.exec_()
+"""
+test.py
+
+  created by:   Tim Clarke
+
+    Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from
+      <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+
+  date:         8jan2020
+  change log:
+  purpose:      testing of pyqt
+
+                pseudo-code:
+                  
+
+  returns:
+  errors:
+  assumes:      
+  side effects:
+"""
+
+import sys
+from PyQt5 import QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class mine(QMainWindow):
+  def __init__(self, parent=None):
+    QMainWindow.__init__(self, parent)
+    uic.loadUi('files/mainwindow.ui', self)
+    self.setWindowIcon(QtGui.QIcon('files/hospital.png'))
+
+if __name__ == '__main__':
+  app = QApplication([])
+
+  window = mine()
+  window.show()
+
+  tblWidgets = window.findChildren(QtWidgets.QTableWidget)
+  for wdg in tblWidgets:
+    print(wdg.objectName())
+  
+  sys.exit(app.exec_())
