ce4a89ba3f496db9fccb20ab4d764371fc86412b
Tim Clarke <tim.clarke@minerva.info>
2020-02-16 18:34:22 +0000

    receive click location on first tab's grid

--- src/main.py
+++ src/main.py
@@ -11,8 +11,7 @@ test.py
 
   created by:   Tim Clarke
 
-    Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from
-      <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    Icons originally made by Freepik from www.flaticon.com
 
   date:         8jan2020
   change log:
@@ -29,7 +28,13 @@ test.py
 
 import sys
 from PyQt5 import QtGui, QtWidgets, uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget
+
+
+# TODO useful in debugging, remove for live
+def dump(obj):
+  for attr in dir(obj):
+    print("obj.%s = %r" % (attr, getattr(obj, attr)))
 
 
 class mine(QMainWindow):
@@ -37,15 +42,22 @@ class mine(QMainWindow):
     QMainWindow.__init__(self, parent)
     uic.loadUi('files/mainwindow.ui', self)
     self.setWindowIcon(QtGui.QIcon('files/hospital.png'))
+    self.setHandlers()
+
+  def setHandlers(self):
+    tblWidget = self.findChild(QTableWidget, 'tblShifts')
+    if tblWidget is not None:
+      tblWidget.clicked.connect(self.alert)
+
+  def alert(self, index):
+    """ argments: self=MainWindow, index=QModelIndex of clicked """
+    print(index.row(), index.column())
+
 
 if __name__ == '__main__':
   app = QApplication([])
 
   window = mine()
   window.show()
-
-  tblWidgets = window.findChildren(QtWidgets.QTableWidget)
-  for wdg in tblWidgets:
-    print(wdg.objectName())
   
   sys.exit(app.exec_())
