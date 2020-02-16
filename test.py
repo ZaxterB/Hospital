a8611ecca1edaa355e786019dfd42ca1dd7bb39b
Tim Clarke <tim.clarke@minerva.info>
2020-02-16 13:32:27 +0000

    final test, migrate to live

--- test.py
+++ test.py
@@ -10,6 +10,10 @@ __version__ = "0.0.1"
 test.py
 
   created by:   Tim Clarke
+
+    Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from
+      <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+
   date:         8jan2020
   change log:
   purpose:      testing of pyqt
@@ -29,14 +33,19 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class mine(QMainWindow):
-    def __init__(self, parent=None):
-        QMainWindow.__init__(self, parent)
-        uic.loadUi('mainwindow.ui', self)
+  def __init__(self, parent=None):
+    QMainWindow.__init__(self, parent)
+    uic.loadUi('mainwindow.ui', self)
+    self.setWindowIcon(QtGui.QIcon('hospital.png')
 
 if __name__ == '__main__':
-    app = QApplication([])
+  app = QApplication([])
 
-    window = mine()
-    window.show()
+  window = mine()
+  window.show()
 
-    sys.exit(app.exec_())
+  tblWidgets = window.findChildren(QtWidgets.QTableWidget)
+  for wdg in tblWidgets:
+    print(wdg.objectName())
+  
+  sys.exit(app.exec_())
