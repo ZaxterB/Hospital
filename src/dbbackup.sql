10c2d058cb61d67fad9719a79b7050ab02bc9c41
Tim Clarke <tim.clarke@minerva.info>
2020-05-23 12:33:15 +0100

    table check for validation

--- src/dbbackup.sql
+++ src/dbbackup.sql
@@ -47,12 +47,12 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.bedevent (
-    bedeventid serial NOT NULL,
+    bedeventid integer NOT NULL,
     eventtime timestamp without time zone NOT NULL,
     eventtype smallint NOT NULL,
-    patientid integer NOT NULL,
+    patientid integer,
     bedid smallint NOT NULL,
-    monitortypeid integer NOT NULL
+    monitortypeid integer
 );
 
 
@@ -63,7 +63,7 @@ ALTER TABLE public.bedevent OWNER TO postgres;
 --
 
 CREATE TABLE public.staffevent (
-    staffeventid serial NOT NULL,
+    staffeventid integer NOT NULL,
     eventtime timestamp without time zone NOT NULL,
     type smallint NOT NULL,
     staffid integer NOT NULL
@@ -96,7 +96,7 @@ ALTER TABLE public."AllEvents" OWNER TO postgres;
 --
 
 CREATE TABLE public.bed (
-    bedid serial NOT NULL,
+    bedid integer NOT NULL,
     bednumber integer NOT NULL
 );
 
@@ -104,11 +104,55 @@ CREATE TABLE public.bed (
 ALTER TABLE public.bed OWNER TO postgres;
 
 --
+-- Name: bed_bedid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.bed_bedid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.bed_bedid_seq OWNER TO postgres;
+
+--
+-- Name: bed_bedid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.bed_bedid_seq OWNED BY public.bed.bedid;
+
+
+--
+-- Name: bedevent_bedeventid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.bedevent_bedeventid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.bedevent_bedeventid_seq OWNER TO postgres;
+
+--
+-- Name: bedevent_bedeventid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.bedevent_bedeventid_seq OWNED BY public.bedevent.bedeventid;
+
+
+--
 -- Name: bedmodule; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.bedmodule (
-    bedmoduleid serial NOT NULL,
+    bedmoduleid integer NOT NULL,
     bedid integer NOT NULL,
     moduleid integer NOT NULL
 );
@@ -117,11 +161,33 @@ CREATE TABLE public.bedmodule (
 ALTER TABLE public.bedmodule OWNER TO postgres;
 
 --
+-- Name: bedmodule_bedmoduleid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.bedmodule_bedmoduleid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.bedmodule_bedmoduleid_seq OWNER TO postgres;
+
+--
+-- Name: bedmodule_bedmoduleid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.bedmodule_bedmoduleid_seq OWNED BY public.bedmodule.bedmoduleid;
+
+
+--
 -- Name: module; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.module (
-    moduleid serial NOT NULL,
+    moduleid integer NOT NULL,
     name character varying NOT NULL
 );
 
@@ -129,11 +195,33 @@ CREATE TABLE public.module (
 ALTER TABLE public.module OWNER TO postgres;
 
 --
+-- Name: module_moduleid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.module_moduleid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.module_moduleid_seq OWNER TO postgres;
+
+--
+-- Name: module_moduleid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.module_moduleid_seq OWNED BY public.module.moduleid;
+
+
+--
 -- Name: modulemonitor; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.modulemonitor (
-    modulemonitorid serial NOT NULL,
+    modulemonitorid integer NOT NULL,
     monitortypeid integer NOT NULL,
     moduleid integer NOT NULL,
     minval numeric NOT NULL,
@@ -144,11 +232,33 @@ CREATE TABLE public.modulemonitor (
 ALTER TABLE public.modulemonitor OWNER TO postgres;
 
 --
+-- Name: modulemonitor_modulemonitorid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.modulemonitor_modulemonitorid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.modulemonitor_modulemonitorid_seq OWNER TO postgres;
+
+--
+-- Name: modulemonitor_modulemonitorid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.modulemonitor_modulemonitorid_seq OWNED BY public.modulemonitor.modulemonitorid;
+
+
+--
 -- Name: monitortype; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.monitortype (
-    monitortypeid serial NOT NULL,
+    monitortypeid integer NOT NULL,
     name character varying NOT NULL,
     unit character varying NOT NULL,
     defaultmax numeric(5,2) NOT NULL,
@@ -161,11 +271,33 @@ CREATE TABLE public.monitortype (
 ALTER TABLE public.monitortype OWNER TO postgres;
 
 --
+-- Name: monitortype_monitortypeid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.monitortype_monitortypeid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.monitortype_monitortypeid_seq OWNER TO postgres;
+
+--
+-- Name: monitortype_monitortypeid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.monitortype_monitortypeid_seq OWNED BY public.monitortype.monitortypeid;
+
+
+--
 -- Name: patient; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.patient (
-    patientid serial NOT NULL,
+    patientid integer NOT NULL,
     name character varying NOT NULL
 );
 
@@ -173,11 +305,33 @@ CREATE TABLE public.patient (
 ALTER TABLE public.patient OWNER TO postgres;
 
 --
+-- Name: patient_patientid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.patient_patientid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.patient_patientid_seq OWNER TO postgres;
+
+--
+-- Name: patient_patientid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.patient_patientid_seq OWNED BY public.patient.patientid;
+
+
+--
 -- Name: shift; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.shift (
-    shiftid serial NOT NULL,
+    shiftid integer NOT NULL,
     staffid integer NOT NULL,
     start timestamp without time zone NOT NULL,
     currentend timestamp without time zone NOT NULL
@@ -187,11 +341,33 @@ CREATE TABLE public.shift (
 ALTER TABLE public.shift OWNER TO postgres;
 
 --
+-- Name: shift_shiftid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.shift_shiftid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.shift_shiftid_seq OWNER TO postgres;
+
+--
+-- Name: shift_shiftid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.shift_shiftid_seq OWNED BY public.shift.shiftid;
+
+
+--
 -- Name: staff; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.staff (
-    staffid serial NOT NULL,
+    staffid integer NOT NULL,
     name character varying NOT NULL,
     email character varying,
     telnumber character varying,
@@ -206,6 +382,120 @@ ALTER TABLE public.staff OWNER TO postgres;
 --
 
 COMMENT ON COLUMN public.staff.stafftype IS '1=nurse, 2=consultant';
+
+
+--
+-- Name: staff_staffid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.staff_staffid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.staff_staffid_seq OWNER TO postgres;
+
+--
+-- Name: staff_staffid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.staff_staffid_seq OWNED BY public.staff.staffid;
+
+
+--
+-- Name: staffevent_staffeventid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.staffevent_staffeventid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.staffevent_staffeventid_seq OWNER TO postgres;
+
+--
+-- Name: staffevent_staffeventid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.staffevent_staffeventid_seq OWNED BY public.staffevent.staffeventid;
+
+
+--
+-- Name: bed bedid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.bed ALTER COLUMN bedid SET DEFAULT nextval('public.bed_bedid_seq'::regclass);
+
+
+--
+-- Name: bedevent bedeventid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.bedevent ALTER COLUMN bedeventid SET DEFAULT nextval('public.bedevent_bedeventid_seq'::regclass);
+
+
+--
+-- Name: bedmodule bedmoduleid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.bedmodule ALTER COLUMN bedmoduleid SET DEFAULT nextval('public.bedmodule_bedmoduleid_seq'::regclass);
+
+
+--
+-- Name: module moduleid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.module ALTER COLUMN moduleid SET DEFAULT nextval('public.module_moduleid_seq'::regclass);
+
+
+--
+-- Name: modulemonitor modulemonitorid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.modulemonitor ALTER COLUMN modulemonitorid SET DEFAULT nextval('public.modulemonitor_modulemonitorid_seq'::regclass);
+
+
+--
+-- Name: monitortype monitortypeid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.monitortype ALTER COLUMN monitortypeid SET DEFAULT nextval('public.monitortype_monitortypeid_seq'::regclass);
+
+
+--
+-- Name: patient patientid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.patient ALTER COLUMN patientid SET DEFAULT nextval('public.patient_patientid_seq'::regclass);
+
+
+--
+-- Name: shift shiftid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.shift ALTER COLUMN shiftid SET DEFAULT nextval('public.shift_shiftid_seq'::regclass);
+
+
+--
+-- Name: staff staffid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.staff ALTER COLUMN staffid SET DEFAULT nextval('public.staff_staffid_seq'::regclass);
+
+
+--
+-- Name: staffevent staffeventid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.staffevent ALTER COLUMN staffeventid SET DEFAULT nextval('public.staffevent_staffeventid_seq'::regclass);
 
 
 --
@@ -288,8 +578,8 @@ COPY public.monitortype (monitortypeid, name, unit, defaultmax, defaultmin, dang
 --
 
 COPY public.patient (patientid, name) FROM stdin;
-1   Alice
-2   Bob
+1	Alice
+2	Bob
 \.
 
 
@@ -323,6 +613,76 @@ COPY public.staff (staffid, name, email, telnumber, stafftype) FROM stdin;
 
 COPY public.staffevent (staffeventid, eventtime, type, staffid) FROM stdin;
 \.
+
+
+--
+-- Name: bed_bedid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.bed_bedid_seq', 1, false);
+
+
+--
+-- Name: bedevent_bedeventid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.bedevent_bedeventid_seq', 1, false);
+
+
+--
+-- Name: bedmodule_bedmoduleid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.bedmodule_bedmoduleid_seq', 1, false);
+
+
+--
+-- Name: module_moduleid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.module_moduleid_seq', 1, false);
+
+
+--
+-- Name: modulemonitor_modulemonitorid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.modulemonitor_modulemonitorid_seq', 1, false);
+
+
+--
+-- Name: monitortype_monitortypeid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.monitortype_monitortypeid_seq', 1, false);
+
+
+--
+-- Name: patient_patientid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.patient_patientid_seq', 1, false);
+
+
+--
+-- Name: shift_shiftid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.shift_shiftid_seq', 1, false);
+
+
+--
+-- Name: staff_staffid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.staff_staffid_seq', 1, false);
+
+
+--
+-- Name: staffevent_staffeventid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.staffevent_staffeventid_seq', 1, false);
 
 
 --
@@ -379,6 +739,21 @@ ALTER TABLE ONLY public.staff
 
 ALTER TABLE ONLY public.bedmodule
     ADD CONSTRAINT bedmodule_pkey PRIMARY KEY (bedmoduleid);
+
+
+--
+-- Name: bedevent eventtype_ck; Type: CHECK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.bedevent
+    ADD CONSTRAINT eventtype_ck CHECK (((eventtype >= 1) AND (eventtype <= 9))) NOT VALID;
+
+
+--
+-- Name: CONSTRAINT eventtype_ck ON bedevent; Type: COMMENT; Schema: public; Owner: postgres
+--
+
+COMMENT ON CONSTRAINT eventtype_ck ON public.bedevent IS 'ensure eventtype has a valid value';
 
 
 --
